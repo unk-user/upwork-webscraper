337ed40ed4ac4b480960f025b67d2ec7c9bbeb52
Hamza <144457566+unk-user@users.noreply.github.com>
2024-11-12 19:55:41 +0100

    chore: update launcher property & request handling for lambda and performance

--- rod.go
+++ rod.go
@@ -6,8 +6,11 @@ import (
 	"io"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/proto"
 )
 
 const (
@@ -34,24 +37,89 @@ func MakeParams(keywords string) string {
 	return "&q=%28" + strings.Join(array, "%20OR%20") + "%29"
 }
 
-func GetNewJobs() {
-	browser := rod.New().MustConnect()
-	defer browser.MustClose()
+func GetNewJobs() (err error) {
+	const (
+		navigationTimeout  = 5 * time.Second
+		requestIdleTimeout = 10 * time.Second
+		htmlTimeout        = 5 * time.Second
+	)
 
-	page := browser.MustPage("")
+	err = rod.Try(func() {
+		launcher := launchInLambda()
 
-	keywords, err := ScanQuery(os.Stdout, os.Stdin)
+		defer launcher.Cleanup()
+
+		defer launcher.Kill()
+
+		u := launcher.MustLaunch()
+
+		browser := rod.New().ControlURL(u).MustConnect()
+		defer browser.MustClose()
+
+		page := browser.MustPage()
+
+		router := page.HijackRequests()
+
+		router.MustAdd("*", func(ctx *rod.Hijack) {
+			if ctx.Request.Type() != proto.NetworkResourceTypeDocument {
+				ctx.Response.Fail(proto.NetworkErrorReasonBlockedByClient)
+				return
+			}
+
+			ctx.ContinueRequest(&proto.FetchContinueRequest{})
+		})
+
+		go router.Run()
+
+		keywords, err := ScanQuery(os.Stdout, os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		fullUrl := URL + MakeParams(keywords)
+		page.Timeout(navigationTimeout).MustNavigate(fullUrl).MustWaitElementsMoreThan(".job-tile", 9)
+
+		waitRequestIdle := page.Timeout(requestIdleTimeout).MustWaitRequestIdle()
+		waitRequestIdle()
+
+		jobTiles := page.Timeout(htmlTimeout).MustElements(".job-tile")
+		for i, jobTile := range jobTiles {
+			title := jobTile.MustElement(".job-tile-title").MustText()
+			uid := *jobTile.MustAttribute("data-ev-job-uid")
+
+			fmt.Printf("%d - %s: %s\n", i, uid, title)
+		}
+	})
+
 	if err != nil {
-		panic(err)
+		return err
 	}
-	fullUrl := URL + MakeParams(keywords)
-	page.MustNavigate(fullUrl).MustWaitElementsMoreThan(".job-tile", 9)
 
-	jobTiles := page.MustElements(".job-tile")
-	for i, jobTile := range jobTiles {
-		title := jobTile.MustElement(".job-tile-title").MustText()
-		uid := *jobTile.MustAttribute("data-ev-job-uid")
+	return nil
+}
 
-		fmt.Printf("%d - %s: %s\n", i, uid, title)
-	}
+func launchInLambda() *launcher.Launcher {
+	return launcher.New().
+		Set("allow-running-insecure-content").
+		Set("autoplay-policy", "user-gesture-required").
+		Set("disable-component-update").
+		Set("disable-domain-reliability").
+		Set("disable-features", "AudioServiceOutOfProcess", "IsolateOrigins", "site-per-process").
+		Set("disable-print-preview").
+		Set("disable-setuid-sandbox").
+		Set("disable-site-isolation-trials").
+		Set("disable-speech-api").
+		Set("disable-web-security").
+		Set("disk-cache-size", "33554432").
+		Set("enable-features", "SharedArrayBuffer").
+		Set("hide-scrollbars").
+		Set("ignore-gpu-blocklist").
+		Set("in-process-gpu").
+		Set("mute-audio").
+		Set("no-default-browser-check").
+		Set("no-pings").
+		Set("no-sandbox").
+		Set("no-zygote").
+		Set("single-process").
+		Set("use-gl", "swiftshader").
+		Set("window-size", "1920", "1080")
 }
