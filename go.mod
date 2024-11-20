dac71b2d7b5740934cf02a0d79f7e532ea7b74fe
Hamza <144457566+unk-user@users.noreply.github.com>
2024-11-20 12:57:42 +0100

    chore: setup lambda function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module upword_webscraper
 
 go 1.23.2
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "\"Hello from Lambda!\"",
+	}
+	return response, nil
+}
+
 func main() {
-	fmt.Printf("Hello, World!\n")
+	lambda.Start(handler)
 }
