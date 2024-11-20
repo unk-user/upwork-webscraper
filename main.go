3aa280cd182ba46858d187cef2c392831252a1e9
Hamza <144457566+unk-user@users.noreply.github.com>
2024-11-20 11:35:17 +0100

    chore: reset main branch code for ci setup

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	// "context"
-	// "fmt"
-
-	// "github.com/aws/aws-lambda-go/events"
-	// "github.com/aws/aws-lambda-go/lambda"
-	"github.com/go-rod/rod/lib/launcher"
+	"fmt"
 )
 
-
 func main() {
-	launcher := launcher.New()
-	_, err := GetNewJobs("react", launcher)
-
-	if err != nil {
-		panic(err)
-	}
+	fmt.Printf("Hello, World!\n")
 }
