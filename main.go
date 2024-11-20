692e9ddef72ed7b2570d6f8ff1005fcda704c6f1
Hamza <144457566+unk-user@users.noreply.github.com>
2024-11-20 20:29:17 +0100

    feat: scrape jobs using category and keywords

--- main.go
+++ main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	payload := Payload{
+		CategoryId: event.QueryStringParameters["categoryId"],
+		Keywords:   event.QueryStringParameters["keywords"],
+	}
+
+	jobs, err := GetNewJobs(payload)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	body, err := json.Marshal(jobs)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "\"Hello from Lambda!\"",
+		Body:       string(body),
 	}
 	return response, nil
 }
